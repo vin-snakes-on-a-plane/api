e485053378e3b6112015fcffb058fd96439d6d83
Marvin Colgin <bpadmin@builtbp.com>
2019-08-20 09:51:29 -0700

    DONE: worked on remainder in class. Refactored all apps into a single.

--- seat/models.py
+++ seat/models.py
@@ -3,10 +3,3 @@ from flight.models import Flight
 from passengers.models import Passenger
 
 
-class Seat(models.Model):
-    name = models.CharField(max_length=256)
-    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
-    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE, default=None)
-
-    def __str__(self):
-        return f'name:[{self.name}] flight:[{self.flight}] passenger:[{self.passenger}]'
